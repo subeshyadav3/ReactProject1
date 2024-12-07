ebf1a2df1e825d17cdca7238626c4968e428e7f9
Subesh Yadav <subeshyadav3@gmail.com>
2024-12-07 12:42:36 +0545

    making

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
        
-        <Route path='/' element={<Layout />}>
+        <Route path='/reactProject1' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='contact' element={<Contact />} />
           <Route path='about' element={<About />} />
